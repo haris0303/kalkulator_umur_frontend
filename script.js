1b292a4d348ff9aff1a0cd4db46fd9186d2383e6
Adil Haris Hamka <harishamka2009@gmail.com>
2026-02-26 23:13:20 +0700

    initial frontend

---
+++ script.js
@@ -0,0 +1,30 @@
+const btnEl = document.getElementById("btn");
+const birthdayEl = document.getElementById("birthday");
+const resultEl = document.getElementById("result");
+
+async function calculateAge() {
+    const birthdayValue = birthdayEl.value;
+
+    if (birthdayValue === "") {
+        alert("Masukkan tanggal lahir dulu");
+        return;
+    }
+
+    try {
+        const response = await fetch("https://kalkulatorumurbackend-production.up.railway.app/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ birthday: birthdayValue })
+        });
+
+        const data = await response.json();
+
+        resultEl.innerText = `Umur kamu ${data.age} tahun`;
+    } catch (error) {
+        resultEl.innerText = "Gagal konek ke server";
+    }
+}
+
+btnEl.addEventListener("click", calculateAge);
